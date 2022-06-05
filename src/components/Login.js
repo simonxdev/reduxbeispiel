f28f7c4114011fdc2517a99e0359866bf3d058cb
Simon Lohmann <simonlohmann@gmx.net>
2022-06-06 00:28:37 +0200

    full

---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useDispatch } from "react-redux"
+import { login, logout } from "../features/user"
+
+function Login() {
+
+const dispatch = useDispatch();
+
+  return (
+    <div>
+      <button onClick={ () => dispatch( login( {name: "Simon", age: 32, email: "simon@gmail.com"} ) ) }>Login</button>
+      <button onClick={ () => dispatch( logout() ) }>LOGOUT</button>
+    </div>
+  )
+}
+
+export default Login
